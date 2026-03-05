cbde245e73428a466fe53154884b89199be01af6
busi-n <khulisanimsipa@gmail.com>
2026-03-05 07:59:46 +0200

    code showing on logcat

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -19,8 +19,8 @@ class MainActivity : AppCompatActivity() {
         val yearOfBirth=2006
         Log.d( "Hi",firstName +" "+lastName+ "you live in"+ address + "it is"+ isMale+"that you are male"+dayOfBirth+" "+
         monthOfBirth+" "+yearOfBirth)
-
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
