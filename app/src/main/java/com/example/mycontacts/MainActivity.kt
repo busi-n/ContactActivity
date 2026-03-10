d7c2ff7f2f613c35ce1ade58b81aae0fa62e0f0a
busi-n <khulisanimsipa@gmail.com>
2026-03-10 10:45:59 +0200

    calculated activities printed

--- app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -17,6 +17,27 @@ class MainActivity : AppCompatActivity() {
         val dayOfBirth=15
         val monthOfBirth="September"
         val yearOfBirth=2006
+        val currentYear=2026 //Based on the current date
+        val age= currentYear- yearOfBirth
+
+        //1. Calculate years until 100
+        val yearsUntil100=100-age
+
+        //2.Age in months
+        val ageInMonths= age*12
+
+        //3.Age in days (excluding leap years)
+        val ageInDays=age*365
+
+        //4. Reminder when divided by 5
+        val ageReminder=age%5
+
+     // Printing the calculated values to Logcat
+        Log.d("Calculation"," Years until 100: $yearsUntil100")
+        Log.d("Calculation"," Age in months: $ageInMonths")
+        Log.d("Calculation"," Age in days: $ageInDays")
+        Log.d("Calculation"," Reminder when divided by 5: $ageReminder")
+
         Log.d( "Hi",firstName +" "+lastName+ "you live in"+ address + "it is"+ isMale+"that you are male"+dayOfBirth+" "+
         monthOfBirth+" "+yearOfBirth)
         setContentView(R.layout.activity_main)
